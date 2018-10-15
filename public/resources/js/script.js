71f189993be0e9beddbdb7f892c7a69ee101d10b
Florence <stamandf@gmail.com>
2018-10-15 17:09:03 -0400

    changed section-cities div and added jQuery to nav menu

---
+++ public/resources/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+          $('nav').addClass('sticky');
+        } else {
+          $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    //   var waypoints = $('#handler-first').waypoint(function(direction) {
+    //   notify(this.element.id + ' hit 25% from top of window')
+    // }, {
+    //   offset: '25%'
+    // })
+
+
+});
